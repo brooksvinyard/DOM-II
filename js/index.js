01b14a9746461c174fa92510c79bbea8f3d53054
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-01-29 12:50:01 -0800

    Mouseover changes picture border radius

--- js/index.js
+++ js/index.js
@@ -6,3 +6,15 @@ const containerSelect = document.querySelector('.home');
 containerSelect.addEventListener('click', e => {
     e.target.style.fontFamily = "Indie Flower";
 });
+
+// 2 - On mouseover / out change the picture border radius
+let adventureImgSelect = document.querySelector('.img-content img');
+
+adventureImgSelect.addEventListener('mouseover', e => {
+    e.target.style.borderRadius =  "100%";
+});
+adventureImgSelect.addEventListener('mouseout', e => {
+    e.target.style.borderRadius =  "10px";
+});
+
+// 3
