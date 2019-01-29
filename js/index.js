99ba22ddd9fbd0abd5f7ddbcb60800b44e62e755
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-01-29 12:49:22 -0800

    Click to change paragraph font on the page

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 // Your code goes here
 
+
+// 1 - Makes text you click change font
+const containerSelect = document.querySelector('.home');
+containerSelect.addEventListener('click', e => {
+    e.target.style.fontFamily = "Indie Flower";
+});
