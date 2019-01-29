dd1b9dce881b750b816fd89a6c9266b1163401fb
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-01-29 13:26:07 -0800

    Change image dropshadow on mouse click and release

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ containerSelect.addEventListener('click', e => {
     e.target.style.fontFamily = "Indie Flower";
 });
 
-// 2 - On mouseover / out change the picture border radius
+// 2/3 - On mouseover / out change the picture border radius
 let adventureImgSelect = document.querySelector('.img-content img');
 
 adventureImgSelect.addEventListener('mouseover', e => {
@@ -17,10 +17,19 @@ adventureImgSelect.addEventListener('mouseout', e => {
     e.target.style.borderRadius =  "10px";
 });
 
-// 3 - Changes navigation background on double click
+// 4 - Changes navigation background on double click
 let navigationSelect = document.querySelector('.main-navigation');
 
 navigationSelect.addEventListener('dblclick', e => {
     e.target.style.background =  "#17A2B8";
 });
 
+// 5/6 - On mouse click / release change the picture drop shadow
+let funImgSelect = document.querySelector('.inverse-content img');
+console.log(funImgSelect);
+funImgSelect.addEventListener('mousedown', e => {
+    e.target.style.boxShadow = "1px 1px 20px 10px #212529";
+});
+funImgSelect.addEventListener('mouseup', e => {
+    e.target.style.boxShadow = "none";
+});
